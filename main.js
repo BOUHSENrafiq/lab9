8cbe8b64b17d727d66456bd796445c6e7d2cdb74
BOUHSENrafiq <58845334+BOUHSENrafiq@users.noreply.github.com>
2020-05-15 13:19:48 +0000

    V1

--- main.js
+++ main.js
@@ -2,9 +2,9 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var etudiants_1 = require("./etudiants");
 var etudiant1 = new etudiants_1.Etudiants(1, "smail");
-etudiant1.notes[0] = 9;
+etudiant1.notes[0] = 8;
 etudiant1.notes.push(10);
-etudiant1.notes.push(10);
+etudiant1.notes.push(2);
 etudiant1.notes.push(11);
 console.log('moyenne = ' + etudiant1.calculerMoyenne());
 console.log('Max = ' + etudiant1.getMax());
